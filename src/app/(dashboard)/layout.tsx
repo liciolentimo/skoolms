5659d4258cfb5ef943de39f015540012996a2402
liciolentimo <lentimo@gmail.com>
2024-09-03 15:21:01 +0300

    responsive left menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/components/Menu";
+
 export default function DashboardLayout({
     children,
   }: Readonly<{
@@ -5,7 +9,20 @@ export default function DashboardLayout({
   }>) {
     return (
       
-        <div>{children}</div>
+        <div className="h-screen flex">
+          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+          <Link 
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2">
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SkoolMS</span>
+          </Link>
+          <Menu />
+          </div>
+          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+
+          </div>
+          </div>
     
     );
   }
