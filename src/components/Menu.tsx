fe6bf04c9dc2f20a4c8bd956d08df318eeff747a
liciolentimo <lentimo@gmail.com>
2024-09-04 16:16:15 +0300

    student page design

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -122,14 +123,20 @@ const Menu = () => {
       {menuItems.map(i=>(
         <div className="flex flex-col gap-2" key={i.title}>
           <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
-          {i.items.map(item=>(
-            <Link
-            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
-             href={item.href} key={item.label}>
-            <Image src={item.icon} alt="" width={20} height={20} />
-            <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {i.items.map(item=>{
+          {
+            if(item.visible.includes(role)){
+              return(
+                <Link
+                className="flex items-center justify-center lg:justify-start gap-4 md:px-2 text-gray-500 py-2 rounded-md hover:bg-mySkyLight"
+                 href={item.href} key={item.label}>
+                <Image src={item.icon} alt="" width={20} height={20} />
+                <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              )
+            }
+          }
+})}
         </div>
       ))}
     </div>
