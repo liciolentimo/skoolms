aa983bb4957f938d15f901a60bf9c8b9d00df537
liciolentimo <lentimo@gmail.com>
2024-09-04 12:07:46 +0300

    added attendance chart

---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Image from 'next/image';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {
+    name: 'Mon',
+    present: 200,
+    absent: 30,
+   
+  },
+  {
+    name: 'Tue',
+    present: 220,
+    absent: 10,
+    
+  },
+  {
+    name: 'Wed',
+    present: 200,
+    absent: 10,
+   
+  },
+  {
+    name: 'Thu',
+    present: 190,
+    absent: 40,
+    
+  },
+  {
+    name: 'Fri',
+    present: 225,
+    absent: 5,
+    
+  },
+  
+];
+
+
+const AttendanceChart = () => {
+    return(
+        <div className='bg-white rounded-lg p-4 h-full'>
+            <div className='flex justify-between items-center'>
+                <h1 className='text-lg font-semibold'>Attendance</h1>
+                <Image
+                src="/moreDark.png"
+                alt=''
+                width={20}
+                height={20} />
+            </div>
+            <ResponsiveContainer width="100%" height="90%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          barSize={20}
+        >
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke='#ddd' />
+          <XAxis dataKey="name" axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false} />
+          <YAxis axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false} />
+          <Tooltip contentStyle={{borderRadius:"10px",borderColor:"lightgray"}} />
+          <Legend align='left' verticalAlign='top'
+          wrapperStyle={{paddingTop:"20px",paddingBottom:"40px"}} />
+          <Bar dataKey="present" fill="#fae27c"  legendType="circle" radius={[10,10,0,0]}  />
+          <Bar dataKey="absent" fill="#c3ebfa"  legendType="circle" radius={[10,10,0,0]}  />
+        </BarChart>
+      </ResponsiveContainer>
+        </div>
+    )
+}
+
+export default AttendanceChart;
