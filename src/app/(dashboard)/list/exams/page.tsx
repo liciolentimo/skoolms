22b9dfc82aeb2316716c28065fcb5aefb93d19e9
liciolentimo <lentimo@gmail.com>
2024-09-08 16:05:35 +0300

    added exams page

--- src/app/(dashboard)/list/exams/page.tsx
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -1,55 +1,49 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { parentsData, role, studentsData, teachersData } from "@/lib/data";
+import { classesData, examsData, lessonsData, parentsData, role, studentsData, subjectsData, teachersData } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import { emit } from "process";
 
-type Parent = {
+type Exam = {
     id: number;
-    name: string;
-    students: string[];
-    phone: string;
-    email?:string;
-    address: string;
+    subject: string;
+    class:string;
+    teacher: string;
+    date: string;
 }
 
 const columns = [
     {
-        header: "Info", accessor: "info"
+        header: "Subject Name", accessor: "name"
     },
     {
-        header: "Student Names", accessor: "students", className:"hidden md:table-cell"
+        header: "Class", accessor: "class", 
     },
     {
-        header: "Email", accessor: "email", className:"hidden lg:table-cell"
+        header: "Teacher", accessor: "teacher", className:"hidden md:table-cell"
     },
     {
-        header: "Phone", accessor: "phone", className:"hidden lg:table-cell"
+        header: "Date", accessor: "date", className:"hidden md:table-cell"
     },
-    {
-        header: "Address", accessor: "address", className:"hidden lg:table-cell"
-    },
+   
     {
         header:"Actions", accessor: "action"
     }
 ]
 
-const ParentListPage = () => {
-    const renderRow = (item:Parent) => (
+const ExamListPage = () => {
+    const renderRow = (item:Exam) => (
         <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-myPurpleLight">
             <td className="flex items-center gap-4 p-4">
+                {item.subject}
                 
-                <div className="flex flex-col">
-                  <h3 className="font-semibold">{item.name}</h3>  
-                  <p className="text-xs text-gray-500">{item?.email}</p>
-                </div>
             </td>
-            <td className="hidden md:table-cell">{item.students.join(",")}</td>
-    
-            <td className="hidden md:table-cell">{item.phone}</td>
-            <td className="hidden md:table-cell">{item.address}</td>
+            <td>{item.class}</td>
+            <td className="hidden md:table-cell">{item.teacher}</td>
+            <td className="hidden md:table-cell">{item.date}</td>
+           
             <td>
                 <div className="flex items-center gap-2">
                 <Link href={`/list/teachers/${item.id}`}>
@@ -68,7 +62,7 @@ const ParentListPage = () => {
     return(
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
             <div className="flex items-center justify-between">
-                <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
+                <h1 className="hidden md:block text-lg font-semibold">All Exams</h1>
                 <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
                     <TableSearch />
                     <div className="flex items-center gap-4 self-end">
@@ -87,10 +81,10 @@ const ParentListPage = () => {
             </div>
             <Table columns={columns}
             renderRow={renderRow}
-            data={parentsData} />
+            data={examsData} />
             <Pagination />
         </div>
     )
 }
 
-export default ParentListPage;
+export default ExamListPage;
